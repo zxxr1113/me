5aaa71c2f4e882e3a6cbdf71b76d67c6bd862ba2
zxxr1113 <149095211+zxxr1113@users.noreply.github.com>
2026-07-19 10:51:33 +0000

    Deploying to gh-pages from @ zxxr1113/me@dc21b84582d9716614424551000d626ecfe6c019 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://zxxr1113.github.io/me/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://zxxr1113.github.io/XiRan-intro/#contact">contact us</a>.'
               }
             ]
           }
